7c20ae9372e8531cf45fa2d1b28f8a6c139b8b9a
Noah van der Aa <ndvdaa@gmail.com>
2019-02-11 15:43:17 +0100

    v1.0.2

--- src/me/noahvdaa/deathspectator/DeathSpectator.java
+++ src/me/noahvdaa/deathspectator/DeathSpectator.java
@@ -4,6 +4,7 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
@@ -16,22 +17,39 @@ public class DeathSpectator extends JavaPlugin implements Listener {
 	public void onEnable() {
 		new Metrics(this);
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		saveDefaultConfig();
 	}
 	
 	@EventHandler
 	public void deathEvent(PlayerDeathEvent e) {
-		BukkitScheduler scheduler = getServer().getScheduler();
-        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-            @Override
-            public void run() {
-                Player p = (Player) e.getEntity();
-                if(p.isDead()) {
-                	p.spigot().respawn();
-                }
-                p.setGameMode(GameMode.SPECTATOR);
-                
-            }
-        }, 1L);
+        Player p = (Player) e.getEntity();
+		if(p.hasPermission("deathspectator.bypass")) return;
+		if(getConfig().getString("method").equalsIgnoreCase("respawn")) {
+			BukkitScheduler scheduler = getServer().getScheduler();
+	        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+	            @Override
+	            public void run() {
+	                if(p.isDead()) {
+	                	p.spigot().respawn();
+	                }
+	                p.setGameMode(GameMode.SPECTATOR);
+	                
+	            }
+	        }, 1L);
+		}
+	}
+	
+	@EventHandler
+	public void damage(EntityDamageEvent e) {
+		Player p = (Player) e.getEntity();
+		if(p.hasPermission("deathspectator.bypass")) return;
+		if(e.getDamage() < p.getHealth()) return;
+		if(getConfig().getString("method").equalsIgnoreCase("instant")) {
+			e.setCancelled(true);
+            p.setHealth(20.0);
+            p.setGameMode(GameMode.SPECTATOR);
+		}
 	}
 	
 }
